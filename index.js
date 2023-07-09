2e2d7662a6315638081d46453ab7a1525cac1841
WagnerChr <wagnerckv@gmail.com>
2023-07-09 02:08:24 -0300

    feat: change banner

--- index.js
+++ index.js
@@ -25,3 +25,48 @@ function copyText(a) {
   });
 
 }
+
+
+const words = ['Amo a Isabella <3', 'Back-End Developer', 'Love programming <3', 'Sei lá kkk', 'Lorem ipsum dolor sit amet', ' consectetur adipiscing elit', 'Back-End Developer'];
+const text = document.querySelector('.word');
+
+const typeWords = async () => {
+  let i = 0;
+  let texto = ''
+  let codeBar = '|'
+
+  const printWord = async (word) => {
+    for (let j = 0; j < word.length; j++) {
+      texto += word[j]
+      console.log(texto)
+      text.textContent = texto + (j === word.length - 1 ? codeBar : '');;
+      await new Promise(r => setTimeout(r, 200));
+    }
+  };
+
+  const toggleBarraVertical = () => {
+    text.textContent = texto + (text.textContent.endsWith(codeBar) ? '' : codeBar);
+    setTimeout(toggleBarraVertical, 500);
+  };
+
+  toggleBarraVertical();
+
+
+  const removeWord = async (word) => {
+    for(let k = 0; k < word.length; k++) {
+      texto = texto.slice(0, -1)
+      text.innerHTML = texto + (text.textContent.endsWith(codeBar) ? '' : codeBar);
+      await new Promise(r => setTimeout(r, 200));
+    }
+  }
+
+  for(i = 0; i < words.length; i++) {
+    await printWord(words[i]);
+    await new Promise(r => setTimeout(r, 3000));
+    await removeWord(words[i])
+    await new Promise(r => setTimeout(r, 200));
+    i == words.length - 1 ? i = - 1 : i = i;
+  }
+};
+
+typeWords();
